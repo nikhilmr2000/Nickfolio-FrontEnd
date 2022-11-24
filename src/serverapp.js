04cd0709259c35bfae75e795c1d6a3582725b2e8
nikhilmr2000 <nickrockz2000@gmail.com>
2022-11-24 22:41:37 +0530

    first commit

--- src/serverapp.js
+++ src/serverapp.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/nickfolio'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/myapp/index.html'));});
+'/dist/nickfolio/index.html'));});
 app.listen(process.env.PORT || 5000);
