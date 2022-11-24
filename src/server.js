e4e64b899d9f0eb9945b4fb24e25d39f2d455282
M.R.Nikhil <60879899+nikhilmr2000@users.noreply.github.com>
2022-11-24 22:59:21 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/nickfolio'));
+app.use(express.static(__dirname + '/dist/portfolio2'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/nickfolio/index.html'));});
+'/dist/portfolio2/index.html'));});
 app.listen(process.env.PORT || 8080);
